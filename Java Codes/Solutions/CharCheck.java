074b554ab8a965c32e19eec74280401578fe584c
Prathamesh Kothalkar <116967053+Prathamesh-Kothalkar@users.noreply.github.com>
2023-11-25 19:55:08 +0530

    Add files via upload

---
+++ Java Codes/Solutions/CharCheck.java
@@ -0,0 +1,32 @@
+/*
+	Problem :- Given a string, find the index of the first character that does not repeat in the string
+	Example:- For the string "aaa", the first non-repeating character is at index 2 (assuming a zero-based index). So, your program would output 2.
+
+*/
+/*
+	author @Prathamesh-Kothalkar
+*/
+class CharCheck{
+
+	public static int isRepeat(String x){
+				String str=x;
+				int len=str.length();
+				char ch;
+				for(int i=0;i<len;i++){
+					ch=str.charAt(i);
+					if(str.indexOf(ch)== str.lastIndexOf(ch)){
+						return i;
+
+					}
+				}
+				return -1;
+		}
+
+	public static void main(String[] agrs){
+			String str="dudbjfiuicu";
+			int isRep = isRepeat(str);
+			System.out.println(isRep);
+		}
+
+
+}
